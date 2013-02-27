3e8de5564a8d8224a48b36b7edb7646426bde419
marcus <marcus85w@hotmail.com>
2013-02-27 21:38:49 +0100

    Common typedefs.
    In order to decrease compile time and simplifie common typedefs and
    forward declarations is saved in common headerfile.

---
+++ typedefs.h
@@ -0,0 +1,12 @@
+#include <memory>
+
+#include <mw/mathvector.h>
+
+#ifndef TYPEDEFS_H
+#define TYPEDEFS_H
+
+class AiBehavior;
+
+typedef std::shared_ptr<AiBehavior> AiBehaviorPtr;
+
+#endif // TYPEDEFS_H
