063c85c09f9a23d7a7b13aed015b9f072c79c295
marcus <mwthinker@yahoo.se>
2013-10-05 20:24:54 +0200

    Fix fatal error.
    Failed to compile! Fixed!

--- src/treedraw.h
+++ src/treedraw.h
@@ -1,18 +1,18 @@
 #ifndef TREEDRAW_H
 #define TREEDRAW_H
 
-#include "graphictask.h"
+#include "task.h"
 #include "gamesprite.h"
 
 namespace zombie {
 
 	class Map;
 
-	class TreeDraw : public GraphicTask {
+	class TreeDraw : public Task {
 	public:
 		TreeDraw(const Map& map);
 		
-		bool draw(double time) override;
+		bool update(double time) override;
 
 		const Map& map_;
 		mw::Sprite tree_;
