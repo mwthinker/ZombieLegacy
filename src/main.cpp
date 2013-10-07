8062d1803cf2daf5d8c4ab2d46da646b5c8f5a38
marcus <mwthinker@yahoo.se>
2013-10-08 00:16:39 +0200

    Loads windows width and height from xml.

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,32 @@
 
 #include <tinyxml2.h>
 
+#include <iostream>
+#include <sstream>
+
+using namespace tinyxml2;
+using namespace std;
+
 int main (int argc, char** argv) {
+
+	// Load XML file.
 	tinyxml2::XMLDocument xmlDoc;
 	xmlDoc.LoadFile("zombie.xml");
+	if (xmlDoc.Error()) {
+		// Failed!
+		return 1;
+	}
+	XMLHandle handleXml(xmlDoc.FirstChildElement("zombie"));
+	XMLElement* element = handleXml.FirstChildElement("settings").FirstChildElement("width").ToElement();
+	std::stringstream stream;
+	stream << element->GetText() << " ";
+	element = handleXml.FirstChildElement("settings").FirstChildElement("height").ToElement();
+	stream << element->GetText() << " ";	
 
-    zombie::ZombieWindow game;
+	int width, height;
+	stream >> width >> height;
+	
+    zombie::ZombieWindow game(width, height, handleXml);
 	game.startLoop();
 
     return 0;
