c2f6de38f1deae9153c265fa9fe599426264416c
marcus <mwthinker@yahoo.se>
2013-10-12 01:28:31 +0200

    Preparation of Unit tests. Windows VS: Test is executed automatically after compilation and the output is in the built terminal.

---
+++ src/test.cpp
@@ -0,0 +1,16 @@
+#define BOOST_TEST_MODULE example
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE( test_suite )
+
+BOOST_AUTO_TEST_CASE( test_case1 )
+{
+    BOOST_ERROR( "some error 1" );
+}
+
+BOOST_AUTO_TEST_CASE( test_case2 )
+{
+    BOOST_ERROR( "some error 2" );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
