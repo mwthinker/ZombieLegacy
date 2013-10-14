05e240d5a5c64338c285323c0d9575d7402c9e85
marcus <mwthinker@yahoo.se>
2013-10-15 00:14:52 +0200

    Load all data in zombie.xml. However, none of data are so far used. The usage of data will be implemented and commited soon.

--- src/test.cpp
+++ src/test.cpp
@@ -3,14 +3,12 @@
 
 BOOST_AUTO_TEST_SUITE( test_suite )
 
-BOOST_AUTO_TEST_CASE( test_case1 )
-{
-    BOOST_ERROR( "some error 1" );
+BOOST_AUTO_TEST_CASE( test_case1 ) {
+	BOOST_REQUIRE(true);
 }
 
-BOOST_AUTO_TEST_CASE( test_case2 )
-{
-    BOOST_ERROR( "some error 2" );
+BOOST_AUTO_TEST_CASE( test_case2 ) {
+	BOOST_ERROR( "some error 2" );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- src/zombiegame.h
+++ src/zombiegame.h
@@ -7,7 +7,32 @@
 #include <SDL.h>
 #include <tinyxml2.h>
 
+#include <map>
+
 namespace zombie {
+
+	class UnitProperties {
+	public:
+		float mass_;
+		float radius_;
+		float life_;
+		float walkingSpeed_;
+		float runningSpeed_;
+		float stamina_;
+		bool infected_;
+	};
+
+	class WeaponProperties {
+	public:
+		float mass_;
+		std::string animation_;
+		float damage_;
+		float timeBetweenShots_;
+		float range_;
+		int clipSize_;
+		std::string shoot_sound_;
+		std::string reload_sound_;
+	};
 
 	// Responsible of loading map, units and initiate all
 	// game related things and to start the game engine.
@@ -36,11 +61,28 @@ namespace zombie {
 		void handleGameEvent(const GameEvent& gameEvent);
 		void handleRemoval(bool& remove, MovingObject* mOb);
 
+		//Handle to first node <zombie>.
+		bool load(tinyxml2::XMLHandle xml);
+		
+		//Handle to first node <maps>.
+		void loadMap(tinyxml2::XMLHandle xml);
+		
+		//Handle to first node <weapons>.
+		void loadWeapons(tinyxml2::XMLHandle xml);
+		
+		//Handle to first node <settings>.
+		void loadSettings(tinyxml2::XMLHandle xml);
+		
+		//Handle to first node <movingObjects>.
+		void loadMovingUnits(tinyxml2::XMLHandle xml);
+				
 		float innerSpawnRadius_;
 		float outerSpawnRadius_;
 
 		ZombieEngine engine_;
-		DevicePtr keyboard1_, keyboard2_;		
+		DevicePtr keyboard1_, keyboard2_;
+		std::map<std::string, WeaponProperties> weapons_;
+		std::map<std::string, UnitProperties> units_;
 	};
 
 } // Namespace zombie_;
