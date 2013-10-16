8766c89b8817136a016bbafa55602982751a98e1
marcus <mwthinker@yahoo.se>
2013-10-17 00:50:26 +0200

    BuildingPropertis can now be loaded from the xml.

--- src/load.h
+++ src/load.h
@@ -12,7 +12,7 @@ namespace zombie {
 	// Throws mw::Exception if the input is null or if the conversion
 	// fails.
 	// E.g. float a = convertFromText<float>("1.2");
-	// Gives output a = 1.2;
+	// Gives output, a = 1.2;
 	template <class Output>
 	Output convertFromText(const char* txt) {
 		if (txt == nullptr) {
@@ -28,10 +28,51 @@ namespace zombie {
 		}
 	}
 
+	std::stringstream& operator>>(std::stringstream& stream, Point& point) {
+		stream >> point.x;
+		stream >> point.y;
+		return stream;
+	}
+
+	// Takes c-string as input and returns the corresponding buildingProperties.
+	// Throws mw::Exception if the input is null or if the conversion
+	// fails.
+	// E.g. BuildingProperties p = convertFromText<BuildingProperties>("POLYGON ((1.2 2,41.2 0.123))");
+	// Gives BuildingProperties: p.points_={Point(1.2,2),Point(41.2,0.123)}; which is vector with the two points.
 	template <>
 	BuildingProperties convertFromText<BuildingProperties>(const char* txt) {
-		BuildingProperties properties_;
-		return properties_;
+		std::stringstream stream(txt);
+		std::string word;
+		
+		if (stream >> word) {
+			if (word == "POLYGON") {
+				BuildingProperties properties;
+				std::string line = stream.str();
+				int index = line.find("((");
+				int index2 = line.rfind("))");
+				
+				// New line is the string between "((" and "))".
+				line = line.substr(index + 2, index2 - index - 2);
+				
+				// Replace all ',' (comma) with whitespace.
+				for (char& chr : line) {
+					if (chr == ',') {
+						chr = ' ';
+					}
+				}
+				Point point;
+				stream.str(line);
+				while (stream >> point) {
+					properties.points_.push_back(point);
+				}
+				return properties;
+			} else {
+				throw mw::Exception("convertFromText failed, unknown geometry: " + word);
+			}
+		} else {
+			throw mw::Exception("convertFromText failed");
+		}
+		throw mw::Exception("convertFromText failed: BuildingProperties");
 	}
 
 } // Namespace zombie.
