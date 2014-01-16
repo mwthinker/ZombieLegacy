839be3d111f0ce21ec3c5b6611c0e62613a3eb16
mwthinker <mwthinker@yahoo.se>
2014-01-16 01:44:52 +0100

    Updated the submodules..
    SdlGui can be used to get a working gui.

--- src/gamefont.cpp
+++ src/gamefont.cpp
@@ -2,6 +2,6 @@
 
 namespace zombie {
 
-	const mw::FontPtr zombie::font15(new mw::Font("fonts/Ubuntu-B.ttf"));
+	const mw::FontPtr zombie::font15(new mw::Font("fonts/Ubuntu-B.ttf", 18));
 
 } // Namespace zombie.
