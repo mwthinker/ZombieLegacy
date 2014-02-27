06be16f62dce06acb32007098dbd93a37725d80a
mwthinker <mwthinker@yahoo.se>
2014-02-27 15:12:21 +0100

    Removed unused textures.
    Causes segmentation error on linux. May be due to the order of
    initialization of static variables, specifically std::cout variable.

--- src/gamesprite.cpp
+++ src/gamesprite.cpp
@@ -5,17 +5,6 @@
 
 namespace zombie {
 
-	const mw::TexturePtr grassTexture(new mw::Texture("images/grass05.png", [](){
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	}));
-
-	const mw::Sprite grassSprite(grassTexture);
-	const mw::Sprite roadSprite("images/roadtile2.jpg");
-	const mw::Sprite treeSprite("images/zombie/objects/tree.png");
-
 	const mw::Sprite bloodSprite("images/blood.png");
 	const mw::Sprite carSprite("images/car.png");
 
--- src/gamesprite.h
+++ src/gamesprite.h
@@ -6,12 +6,6 @@
 
 namespace zombie {
 
-	extern const mw::TexturePtr grassTexture;
-	
-	extern const mw::Sprite grassSprite;
-	extern const mw::Sprite roadSprite;
-	extern const mw::Sprite treeSprite;
-
 	extern const mw::Sprite bloodSprite;
 	extern const mw::Sprite carSprite;
 
