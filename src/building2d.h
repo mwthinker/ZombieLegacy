40629d9d404cb3997465dbad8bf93b7beb401f0a
anders <jurisoo@hotmail.com>
2014-07-18 15:20:44 +0200

    Pavement solution for 3D buildings.
    map.xml:
    Object type "pavement" introduced.
    Only one building and one pavement are available with a closeby spawning
    point. The pavemnt extends the building with 1 meter in all directions.
    The original map is saved in map_backup.xml
    
    Function draw in building2d:
    One code snippet for drawing the footprint of a gemetry. Usable for
    pavement.
    One codesnippet for drawing fake 3d buildings.

--- src/building2d.h
+++ src/building2d.h
@@ -17,6 +17,16 @@ namespace zombie {
 
 		void draw(float time) override {
 			auto& corners = getCorners();
+			/*
+			
+			glColor3d(0.2, 0.3, 0.9);
+			glBegin(GL_POLYGON);
+
+			for (unsigned int i = 0; i < 3 && i < corners.size(); ++i) {
+				glVertex2d(corners[i].x, corners[i].y);
+			}
+			glEnd();
+			
 			Position delta(0, 0);
 			for (unsigned int i = 0; i < 3 && i < corners.size(); ++i) {
 				delta += 1.f / corners.size() * corners[i];
@@ -26,13 +36,32 @@ namespace zombie {
 			glTranslate2f(delta);
 			glScalef(1, 1, 1);
 			glTranslate2f(-delta);
-			glBegin(GL_TRIANGLES);
+			*/
+			// DRAW BUILDING FOOTPRINT (USE THIS FOR PAVEMENT!)
+			glBegin(GL_POLYGON);
 
-			for (unsigned int i = 0; i < 3 && i < corners.size(); ++i) {
+			for (unsigned int i = 0; i < 4 && i < corners.size(); ++i) {
 				glVertex2d(corners[i].x, corners[i].y);
 			}
-			glEnd();
-			glPopMatrix();
+			glEnd(); 
+			// glPopMatrix();
+
+			// DRAW FAKE 3D BUILDING WITH HEIGHT = z 
+			// (WHITOUT CORNER OUTLINES)
+			int z = 3;
+			for (unsigned int i = 0; i < 3 && i < corners.size(); ++i) {
+				glBegin(GL_POLYGON);
+				glVertex2d(corners[i].x, corners[i].y);
+				glVertex2d(corners[i+1].x, corners[i+1].y);
+				glVertex2d(corners[i + 1].x, corners[i + 1].y+z);
+				glVertex2d(corners[i].x, corners[i].y+z);
+				glVertex2d(corners[i].x, corners[i].y);
+				glEnd();
+			}
+			
+			
+			
+
 		}
 	};
 
