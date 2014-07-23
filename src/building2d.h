6dd31ccd9435ab0d5e99d79191bffef781e05b3c
mwthinker <mwthinker@yahoo.se>
2014-07-23 20:17:03 +0200

    Added random height to 3d buildings.
    And some cleenup!

--- src/building2d.h
+++ src/building2d.h
@@ -13,96 +13,67 @@ namespace zombie {
 	class Building2D : public Building {
 	public:
 		Building2D(const std::vector<Position>& corners) : Building(corners) {
+			height_ = 2 + random() * 3;
 		}
 
 		void draw(float accumulator, float timeStep) override {
 			auto& corners = getCorners();
-			/*
-
-			glColor3d(0.2, 0.3, 0.9);
+			// Draw building footprint (Use this for the pavement!)
+			glColor3f(0.3f, 0.3f, 0.3f);
 			glBegin(GL_POLYGON);
-
-			for (unsigned int i = 0; i < 3 && i < corners.size(); ++i) {
-			glVertex2d(corners[i].x, corners[i].y);
-			}
-			glEnd();
-
-			Position delta(0, 0);
-			for (unsigned int i = 0; i < 3 && i < corners.size(); ++i) {
-			delta += 1.f / corners.size() * corners[i];
-			}
-			glPushMatrix();
-			glColor3d(0.5, 0.5, 0.5);
-			glTranslate2f(delta);
-			glScalef(1, 1, 1);
-			glTranslate2f(-delta);
-			*/
-			// DRAW BUILDING FOOTPRINT (USE THIS FOR PAVEMENT!)
-			glColor3f(0.3, 0.3, 0.3);
-			glBegin(GL_POLYGON);
-
 			for (unsigned int i = 0; i < 4 && i < corners.size(); ++i) {
 				glVertex2d(corners[i].x, corners[i].y);
 			}
 			glEnd();
 			
-			// glPopMatrix();
-			
-			// DRAW FAKE 3D BUILDING WITH HEIGHT = z 
-			// (WHITOUT CORNER OUTLINES)
-			int z = 3;
+			// Draw fake 3D building with height (Without corner outlines).
 			for (unsigned int i = 0; i < 3 && i < corners.size(); ++i) {
 				glBegin(GL_POLYGON);
 				glVertex2d(corners[i].x, corners[i].y);
 				glVertex2d(corners[i + 1].x, corners[i + 1].y);
-				glVertex2d(corners[i + 1].x, corners[i + 1].y + z);
-				glVertex2d(corners[i].x, corners[i].y + z);
+				glVertex2d(corners[i + 1].x, corners[i + 1].y + height_);
+				glVertex2d(corners[i].x, corners[i].y + height_);
 				glVertex2d(corners[i].x, corners[i].y);
 				glEnd();
 			}
 			
-			// DRAW FAKE 3D BUILDING OUTLINES WITH HEIGHT = z
-			glLineWidth(0.9);
-			glColor3f(0.5, 0.5, 0.5);
+			// Draw fake 3D building outlines with height.
+			glColor3f(0.5f, 0.5f, 0.5f);
 			glBegin(GL_LINES);
 			glVertex2d(corners[0].x, corners[0].y);
 			glVertex2d(corners[1].x, corners[1].y);
-			glEnd();
-			glBegin(GL_LINES);
+			
 			glVertex2d(corners[0].x, corners[0].y);
 			glVertex2d(corners[3].x, corners[3].y);
-			glEnd();
-			glBegin(GL_LINES);
-			glVertex2d(corners[0].x, corners[0].y + z);
-			glVertex2d(corners[1].x, corners[1].y + z);
-			glEnd();
-			glBegin(GL_LINES);
-			glVertex2d(corners[0].x, corners[0].y + z);
-			glVertex2d(corners[3].x, corners[3].y + z);
-			glEnd();
-			glBegin(GL_LINES);
+			
+			glVertex2d(corners[0].x, corners[0].y + height_);
+			glVertex2d(corners[1].x, corners[1].y + height_);
+			
+			glVertex2d(corners[0].x, corners[0].y + height_);
+			glVertex2d(corners[3].x, corners[3].y + height_);
+			
 			glVertex2d(corners[3].x, corners[3].y);
-			glVertex2d(corners[3].x, corners[3].y + z);
-			glEnd();
-			glBegin(GL_LINES);
+			glVertex2d(corners[3].x, corners[3].y + height_);
+			
 			glVertex2d(corners[0].x, corners[0].y);
-			glVertex2d(corners[0].x, corners[0].y + z);
-			glEnd();
-			glBegin(GL_LINES);
+			glVertex2d(corners[0].x, corners[0].y + height_);
+			
 			glVertex2d(corners[1].x, corners[1].y);
-			glVertex2d(corners[1].x, corners[1].y + z);
+			glVertex2d(corners[1].x, corners[1].y + height_);
+			
+			glVertex2d(corners[3].x, corners[3].y + height_);
+			glVertex2d(corners[2].x, corners[2].y + height_);
+			
+			glVertex2d(corners[1].x, corners[1].y + height_);
+			glVertex2d(corners[2].x, corners[2].y + height_);
 			glEnd();
-			glBegin(GL_LINES);
-			glVertex2d(corners[3].x, corners[3].y + z);
-			glVertex2d(corners[2].x, corners[2].y + z);
-			glEnd();
-			glBegin(GL_LINES);
-			glVertex2d(corners[1].x, corners[1].y + z);
-			glVertex2d(corners[2].x, corners[2].y + z);
-			glEnd();
-			//
 		}
+	
+	private:
+		float height_;
 	};
+
+
 
 } // Namespace zombie.
 
